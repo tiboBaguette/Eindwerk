67c58874b571734f3952a6a11796fd1662945ef3
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2021-01-28 11:07:54 +0100

    terugkoppeling vereist ivm met registratie data en de juiste databinding in het formulier op de front end

--- frontend/src/app/register-form/register-form.component.ts
+++ frontend/src/app/register-form/register-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../model/User';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserService} from '../service/user.service';
 
 @Component({
   selector: 'app-register-form',
@@ -6,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
-
-  constructor() { }
+  user: User;
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private userService: UserService) {
+    this.user = new User();
+  }
 
   ngOnInit(): void {
   }
--- frontend/src/app/service/user.service.ts
+++ frontend/src/app/service/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../model/User';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
 
-  constructor() { }
+  private readonly userLoginUrl = 'http://localhost:8080/user/login';
+  private readonly userRegisterUrl = 'http://localhost:8080/user/register';
+  constructor(private http: HttpClient) { }
+
+  public userLogin(user: User): Observable<User> {
+    return this.http.post<User>(this.userLoginUrl, user);
+  }
+  public userRegister(user: User): Observable<User> {
+    return this.http.post<User>(this.userRegisterUrl, user);
+  }
 }
