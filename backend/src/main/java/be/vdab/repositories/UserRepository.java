8b1d0015f34f7d708c40c22b5dd7e80cb4b39b72
wouterKenis1 <kenis.wouter@gmail.com>
2021-01-28 10:33:27 +0100

    tests fail - DO NOT MERGE

---
+++ backend/src/main/java/be/vdab/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package be.vdab.repositories;
+
+import be.vdab.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
