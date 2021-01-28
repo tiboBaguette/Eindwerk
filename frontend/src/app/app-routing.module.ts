9dec667c56dedc9fa6ed78e6d3f97c111e15b9d7
Tibo Baguette <tibosid@hotmail.be>
2021-01-28 16:21:15 +0100

    merge login / register / create / list

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
-import {CreatePostComponent} from './create-post/create-post.component';
 import {ListPostComponent} from './components/list-post/list-post.component';
+import {CreatePostComponent} from './components/create-post/create-post.component';
 
 const routes: Routes = [
   {path: 'login-form', component: LoginFormComponent},
