b87f3c8be22cf1295ec90515dbf0fef7bacb8648
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2021-01-28 10:22:17 +0100

    added register form and login form + styling

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomepageComponent} from './homepage/homepage.component';
-import { LoginFormComponent} from './login-form/login-form.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
 const routes: Routes = [
   {path: 'login-form', component: LoginFormComponent},
+  {path: 'register-form', component: RegisterFormComponent},
   {path: '', component: HomepageComponent},
 
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomepageComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
