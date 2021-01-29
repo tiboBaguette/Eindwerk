27e54e56dd6e618e0393e6aa9696183c1c1a5318
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2021-01-29 15:39:55 +0100

    added comment service

---
+++ frontend/src/app/service/comment.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  public comment: Comment | undefined;
+  constructor(private http: HttpClient) { }
+  public createComment(comment: Comment): Observable<Comment> {
+    return this.http.post<Comment>('http://localhost:8080/api/comments/add', comment);
+  }
+}
