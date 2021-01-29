1c210fd8ae012bdac67fbf4387f2eb06e8177d5f
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2021-01-29 13:42:40 +0100

    post-details component rendering on front end

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomepageComponent } from './components/homepage/homepage.component';
-import { LoginFormComponent } from './components/login-form/login-form.component';
-import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ListPostComponent } from './components/list-post/list-post.component';
+import {RegisterFormComponent} from './components/register-form/register-form.component';
+import {HomepageComponent} from './components/homepage/homepage.component';
+import {LoginFormComponent} from './components/login-form/login-form.component';
+import {CreatePostComponent} from './components/create-post/create-post.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import { ListCategoriesComponent } from './components/list-categories/list-categories.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { ListPostComponent } from './components/list-post/list-post.component';
     HomepageComponent,
     LoginFormComponent,
     RegisterFormComponent,
-    ListPostComponent
+    CreatePostComponent,
+    ListPostComponent,
+    ListCategoriesComponent,
+    PostDetailsComponent
   ],
   imports: [
     BrowserModule,
