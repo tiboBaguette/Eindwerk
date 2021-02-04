4644b50c4f7be7ace7fd517137097364f8835be2
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2021-02-04 13:55:10 +0100

    design update create post button on logged in user

--- frontend/src/app/components/list-post/list-post.component.ts
+++ frontend/src/app/components/list-post/list-post.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentChecked, Component, OnInit} from '@angular/core';
 import {Post} from '../../model/Post';
 import {PostService} from '../../service/post.service';
 import {HttpErrorResponse} from '@angular/common/http';
+import {UserService} from "../../service/user.service";
+import {User} from "../../model/User";
 
 @Component({
   selector: 'app-list-post',
   templateUrl: './list-post.component.html',
   styleUrls: ['./list-post.component.css']
 })
-export class ListPostComponent implements OnInit {
+export class ListPostComponent implements OnInit, AfterContentChecked {
   posts: Post[] = [];
-
+  isLoggedIn: boolean | undefined;
+  user: User | undefined;
   constructor(
     private postService: PostService,
+    private userService: UserService
   ) {}
 
   ngOnInit(): void {
+
     this.postService.getPosts().subscribe(
       response => this.posts = response,
       error => this.handleError(error),
     );
   }
 
+  ngAfterContentChecked(): void {
+    this.user = this.userService.user;
+    this.isLoggedIn = this.user !== undefined;
+  }
   handleError(error: HttpErrorResponse): void {}
 }
