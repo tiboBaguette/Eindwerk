2ab804f6e2bef6235e09032c40fd495a8f7a5ab1
Tibo Baguette <tibosid@hotmail.be>
2021-02-05 11:08:36 +0100

    show category name on list posts by category

--- frontend/src/app/components/list-post/list-post.component.ts
+++ frontend/src/app/components/list-post/list-post.component.ts
@@ -14,6 +14,7 @@ import {ActivatedRoute} from '@angular/router';
 export class ListPostComponent implements OnInit {
   posts: Post[] = [];
   isLoggedIn: boolean | undefined;
+  showCategoryName: boolean | undefined;
   user: User | undefined;
 
   constructor(
@@ -33,6 +34,7 @@ export class ListPostComponent implements OnInit {
           error => this.handleError(error),
         );
       } else {
+        this.showCategoryName = true;
         this.postService.getPostByCategory(params.category).subscribe(
           response => this.posts = response,
           error => this.handleError(error),
